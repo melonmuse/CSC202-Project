146a62da1adf2779db5cbc2def6a3fedbfe94df5
Abeer Abu Shiekah <136252883+melonmuse@users.noreply.github.com>
2026-06-01 12:42:28 +0000

    Setting the main file for testing

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello");
+        System.out.println("Testing things out here!");
+        int num =2;
+        if(num==2) {
+            System.out.println("It is 2");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+}
